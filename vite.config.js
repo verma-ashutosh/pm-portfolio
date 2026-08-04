4e826b6f06dd6797099cd9f4cfd3eafd1a867afe
Ashutosh Verma <azhutosh.verma32@gmail.com>
2026-08-04 14:45:38 +0530

    Deploy portfolio source code to renamed repo ashutosh-verma-portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/ashutosh-verma-portfolio/',
   server: {
     port: 5173,
     host: '127.0.0.1'
